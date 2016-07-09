9aee9f6147fdcd4da8c66cc3de549c76e16004bc
Winston Romero <winstonromero@gmail.com>
2016-07-09 11:07:17 -0500

    menu 1

--- inicio.php
+++ inicio.php
@@ -26,24 +26,86 @@ include_once("sesion.php");
         </div>
       </div>
 
-        <nav class="top-bar" data-topbar>
-          <ul class="title-area">
-            <li class="name">
-              <!-- Titulo del Menu -->
-              <h1><a href="#">Inventario de Hardware</a></h1>
-            </li>
-              <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+        
+        <div class ="contain-to-grid sticky">
+          <nav class="top-bar" data-topbar> <!-- Menú de navegación  -->
+            <ul class="title-area">
+              <li class="name">
+                <h1><a href="#">Inventario de Hardware</a></h1> <!-- Titulo menú de navegación  -->
+              </li>
+              <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li> 
             </ul>
             <section class="top-bar-section">
-              <!-- Right Nav Section -->
               <ul class="right">
-                <!--<li><a href="agregar_activo.php">Agregar</a></li> -->
-                <li><a href="form_mantenimiento.php">Mantenimiento</a></li>
-                <li><a href="acceso-php_logout.php">Cerrar sesion</a></li>
+                  <li class="has-dropdown"><a href="#">Activos</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="activo-form_ingresar_activo.php" >Ingresar / Modificar</a></li>
+                      <li><a href="activo-form_exportar_reubicacion.php" >Exportar Reubicado</a></li>
+                      <li><a href="activo-form_consultar_activo.php" >Consultar Activo</a></li>
+                      <li><a href="activo-form_consultar_equipos_por_ubicacion.php" >Consultar por Ubicación</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="has-dropdown"><a href="#">Redes</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="#" >*** Menu de Llaves ***</a></li>
+                       <li><a href="llaves-form_ingresar_prestamo.php" >Ingresar Prestamo</a></li>
+                      <li><a href="llaves-form_consultar_prestamo.php" >Consultar y Recibir</a></li>
+
+                      <li><a href="#" >*** Menu Puntos de red ***</a></li>
+                      <li><a href="red-form_ingresar_punto_de_red.php" >Ingresar Puntos de red</a></li>
+                      <li><a href="red-form_ingresar_punto_de_red_por_lotes.php" >Consultar / Modificar</a></li>
+                      
+                      <li><a href="#" >*** Menu de Switches ***</a></li>
+                      <li><a href="switches-form_ingresar_switches.php" >Ingresar / Modificar</a></li>
+                      <li><a href="switches-form_consultar_switches.php" >Consultar</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="has-dropdown"><a href="#">Compras</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="compras-form_ingresar_compras.php" >Ingresar Nueva Compra</a></li>
+                      <li><a href="compras-form_modificar_compras.php" >Modificar Compra por Activo</a></li>
+                      <li><a href="compras-form_consultar_equipos_por_orden_compra.php" >Consultar por Criterios</a></li>
+                      <li><a href="compras-form_modificar_estado_de_equipos_por_lotes.php" >Modificar Estado por Lotes</a></li>
+                      <li><a href="frmbitacora.php" > Consultar Bitacora</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="has-dropdown"><a href="#">Reubicaciones</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="reubicacion-form_ingresar_reubicacion.php" >Ingresar</a></li>
+                      <li><a href="reubicacion-form_modificar_reubicacion.php" >Consultar / Modificar</a></li>
+                      <li><a href="reubicacion-form_consultar_equipos_por_estado.php" >Consultar por Criterios</a></li>
+                      <li><a href="frmbitacora.php" > Consultar Bitacora</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="has-dropdown"><a href="#">Soportes y Alquilados</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="#" >*** Equipos de Soporte ***</a></li>
+                      <li><a href="soporte-form_ingresar_activo_soporte_a_bodega.php" >Ingresar Equipo Soporte a Bodega</a></li>
+                      <li><a href="soporte-form_ingresar_prestamo_equipo_soporte.php" >Prestar Equipo de Soporte por Nro de Activo</a></li>
+                      <li><a href="soporte-form_consultar_equipos_soporte_en_prestamo.php" >Consultar y recibir Equipos Prestados</a></li>
+
+                      <li><a href="#" >*** Equipos Alquilados ***</a></li>
+                      <li><a href="alquilado-form_ingresar_activo_alquilado_a_bodega.php" >Ingresar Nuevo Equipo Alquilado a Bodega</a></li>
+                      <li><a href="alquilado-form_ingresar_prestamo_equipo_alquilado.php" >Prestar Equipo de Alquilado por Nro de Activo</a></li>
+                      <li><a href="soporte-form_consultar_equipos_soporte_en_prestamo.php" >Consultar y recibir Equipos Alquilados</a></li>
+                      
+                      <li><a href="#" >*** Switches de Soporte ***</a></li>
+                      <li><a href="soporte-form_ingresar_activo_soporte_a_bodega.php" >Ingresar Nuevo Switch de Soporte a Bodega</a></li>
+                      <li><a href="soporte-form_ingresar_prestamo_soporte_switch.php" >Prestar Switch por Nro de Activo</a></li>
+                      <li><a href="soporte-form_consultar_equipos_soporte_en_prestamo.php" >Consultar y recibir Switches Prestados</a></li>
+
+                    </ul>
+                  </li>
+                  <li><a href="acceso-php_logout.php">Cerrar sesion</a></li>
+
               </ul>
             </section>
-        </nav>
-        <br>
+          </nav>
+        </div>
 
         <div class="row">
           <div class="columns large-12">
--- switches-form_ingresar_switches.php
+++ switches-form_ingresar_switches.php
@@ -47,13 +47,71 @@ include_once("sesion.php");
             </ul>
             <section class="top-bar-section">
               <ul class="right">
-                  <li class="has-dropdown"><a href="#">Inicio</a> <!-- Menu lado derecho -->
+                  <li class="has-dropdown"><a href="#">Activos</a> <!-- Menu lado derecho -->
                     <ul class="dropdown">
-                      <li><a href="#">Item 1A</a></li>
-                      <li><a href="#">Item 2A</a></li>
-                      <li><a href="#">Item 3A</a></li>
+                      <li><a href="activo-form_ingresar_activo.php" >Ingresar / Modificar</a></li>
+                      <li><a href="activo-form_exportar_reubicacion.php" >Exportar Reubicado</a></li>
+                      <li><a href="activo-form_consultar_activo.php" >Consultar Activo</a></li>
+                      <li><a href="activo-form_consultar_equipos_por_ubicacion.php" >Consultar por Ubicación</a></li>
                     </ul>
                   </li>
+
+                  <li class="has-dropdown"><a href="#">Redes</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="#" >*** Menu de Llaves ***</a></li>
+                       <li><a href="llaves-form_ingresar_prestamo.php" >Ingresar Prestamo</a></li>
+                      <li><a href="llaves-form_consultar_prestamo.php" >Consultar y Recibir</a></li>
+
+                      <li><a href="#" >*** Menu Puntos de red ***</a></li>
+                      <li><a href="red-form_ingresar_punto_de_red.php" >Ingresar Puntos de red</a></li>
+                      <li><a href="red-form_ingresar_punto_de_red_por_lotes.php" >Consultar / Modificar</a></li>
+                      
+                      <li><a href="#" >*** Menu de Switches ***</a></li>
+                      <li><a href="switches-form_ingresar_switches.php" >Ingresar / Modificar</a></li>
+                      <li><a href="switches-form_consultar_switches.php" >Consultar</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="has-dropdown"><a href="#">Compras</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="compras-form_ingresar_compras.php" >Ingresar Nueva Compra</a></li>
+                      <li><a href="compras-form_modificar_compras.php" >Modificar Compra por Activo</a></li>
+                      <li><a href="compras-form_consultar_equipos_por_orden_compra.php" >Consultar por Criterios</a></li>
+                      <li><a href="compras-form_modificar_estado_de_equipos_por_lotes.php" >Modificar Estado por Lotes</a></li>
+                      <li><a href="frmbitacora.php" > Consultar Bitacora</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="has-dropdown"><a href="#">Reubicaciones</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="reubicacion-form_ingresar_reubicacion.php" >Ingresar</a></li>
+                      <li><a href="reubicacion-form_modificar_reubicacion.php" >Consultar / Modificar</a></li>
+                      <li><a href="reubicacion-form_consultar_equipos_por_estado.php" >Consultar por Criterios</a></li>
+                      <li><a href="frmbitacora.php" > Consultar Bitacora</a></li>
+                    </ul>
+                  </li>
+
+                  <li class="has-dropdown"><a href="#">Soportes y Alquilados</a> <!-- Menu lado derecho -->
+                    <ul class="dropdown">
+                      <li><a href="#" >*** Equipos de Soporte ***</a></li>
+                      <li><a href="soporte-form_ingresar_activo_soporte_a_bodega.php" >Ingresar Equipo Soporte a Bodega</a></li>
+                      <li><a href="soporte-form_ingresar_prestamo_equipo_soporte.php" >Prestar Equipo de Soporte por Nro de Activo</a></li>
+                      <li><a href="soporte-form_consultar_equipos_soporte_en_prestamo.php" >Consultar y recibir Equipos Prestados</a></li>
+
+                      <li><a href="#" >*** Equipos Alquilados ***</a></li>
+                      <li><a href="alquilado-form_ingresar_activo_alquilado_a_bodega.php" >Ingresar Nuevo Equipo Alquilado a Bodega</a></li>
+                      <li><a href="alquilado-form_ingresar_prestamo_equipo_alquilado.php" >Prestar Equipo de Alquilado por Nro de Activo</a></li>
+                      <li><a href="soporte-form_consultar_equipos_soporte_en_prestamo.php" >Consultar y recibir Equipos Alquilados</a></li>
+                      
+                      <li><a href="#" >*** Switches de Soporte ***</a></li>
+                      <li><a href="soporte-form_ingresar_activo_soporte_a_bodega.php" >Ingresar Nuevo Switch de Soporte a Bodega</a></li>
+                      <li><a href="soporte-form_ingresar_prestamo_soporte_switch.php" >Prestar Switch por Nro de Activo</a></li>
+                      <li><a href="soporte-form_consultar_equipos_soporte_en_prestamo.php" >Consultar y recibir Switches Prestados</a></li>
+
+                    </ul>
+                  </li>
+                  <li><a href="acceso-php_logout.php">Cerrar sesion</a></li>
+
               </ul>
             </section>
           </nav>
