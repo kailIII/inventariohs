9c0c094ac975e4bf5aa80961ab856cea4f7b6edd
Winston Elias Romero Duarte <winstonromero@gmail.com>
2016-08-31 10:54:36 -0500

    mail4

--- compras-php_modificar_compras_enviar_email.php
+++ compras-php_modificar_compras_enviar_email.php
@@ -103,10 +103,12 @@
     //Set who the message is to be sent to
     $mail->addAddress("trasladosybajas@uninorte.edu.co", "Traslados y Bajas Uninorte");
     $mail->AddCC("recepciondeactivos@uninorte.edu.co", "Recepcion de Activos");
-    $mail->AddCC($_SESSION['correo'], $_SESSION['nombre']);
-   // $mail->AddCC("weromero@uninorte.edu.co", "Winston Elias Romero Duarte");
     $mail->AddCC("coordinadorequipoinformatico@uninorte.edu.co>", "Coordinador Equipo Informatico");
     $mail->AddCC("arellanaa@uninorte.edu.co>", "Alvaro Ivan Santiago Arellana");
+    $mail->AddCC($_SESSION['correo'], $_SESSION['nombre']);
+    $mail->AddCC("weromero@uninorte.edu.co", "Winston Elias Romero Duarte");
+    //$mail->AddCC("coordinadorequipoinformatico@uninorte.edu.co>", "Coordinador Equipo Informatico");
+    //$mail->AddCC("arellanaa@uninorte.edu.co>", "Alvaro Ivan Santiago Arellana");
     //Set the subject line
     $mail->Subject = 'ENTREGA DE ACTIVO :  ' . "$activo_equipo";
     //Read an HTML message body from an external file, convert referenced images to embedded,
